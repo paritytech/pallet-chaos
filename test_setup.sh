894cc202f0e79629da8a189d14b8882351167b5c
Bernardo A. Rodrigues <bernardo@parity.io>
2021-11-07 02:39:52 -0300

    lint test_setup.sh

--- test_setup.sh
+++ test_setup.sh
@@ -4,8 +4,7 @@ SUBSTRATE_V="3.0.0"
 SUBSTRATE_TIMESTAMP="monthly-2021-10"
 SUBSTRATE_NODE_TEMPLATE_URL="https://github.com/substrate-developer-hub/substrate-node-template/archive/refs/tags/v${SUBSTRATE_V}+${SUBSTRATE_TIMESTAMP}.tar.gz"
 
-echo "-----------------------------------------------------------------------"
-echo "                      Substrate Chaos"
+echo "                         SuBsTrAtE"
 echo "                             _
                           α␡%:ωɚʵ
                       α␡%:       :ωɚʵ
@@ -23,23 +22,35 @@ echo "                             _
                       :ωɚʵ       α␡%:
                           :ɀɀαɀɀ:
                              _
+                           ChA0S
                              "
-echo "We will set up a local test environment based on:"
+echo "We're about to break some chains! Our local setup will be based on:"
 echo " - [substrate-node-template](https://github.com/substrate-developer-hub/substrate-node-template)"
 echo " - [pallet-chaos](https://github.com/paritytech/pallet-chaos)"
+
 echo ""
+echo "Let's clone substrate-node-template..."
 
 rm -rf substrate-node-template
 git clone https://github.com/substrate-developer-hub/substrate-node-template -b v${SUBSTRATE_V}+${SUBSTRATE_TIMESTAMP} substrate-node-chaos
+pushd substrate-node-chaos
 
-pushd substrate-node-template
+echo ""
+echo "Let's clone pallet-chaos..."
 pushd pallets
 git clone ssh://git@github.com/paritytech/pallet-chaos.git
 popd
 
-pwd
+echo ""
+echo "Let's add pallet-chaos to the runtime..."
 git apply pallets/pallet-chaos/diff/add_chaos_runtime.diff
+
+echo ""
+echo "Let's build the node-chaos executable:q:q..."
 cargo build --release
+
+echo ""
+echo "Let's start the node-chaos executable"
 ./target/release/node-template --dev --tmp
 
 
